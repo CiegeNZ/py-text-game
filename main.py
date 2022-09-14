9d4e5baca6c1098425612e0b04c377313aa1694d
Conner Farrant <Conner_f@outlook.com>
2022-09-14 15:34:14 +1200

    make things

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import random
 import sys
 import time
+from pynput import keyboard
+
+# global vars
+things = 0
 
 def spinning_cursor():
     while True:
@@ -41,18 +45,7 @@ def clear():
     print(chr(27) + "[2J")
     print(chr(27) + "[1;1f")
 
-def main():
-    clear()
-    printSlow(1000, "---------------------------------------------------------------------------------------------------------------------------")
-    print("\n")
-    printSlow(100, "------------ ------------------ ------- | Loading Engine Logic |----    -------   - -----        ---    -         -     ---")
-    print("\n")
-    printSlow(1000, "---------------------------------------------------------------------------------------------------------------------------")
-    print("\n")
-    print("\n")
-    print("\n")
-    RandomLoad()
-    time.sleep(3)
+def intro():
     clear()
     printSlow(500, "Welcome to the game")
     time.sleep(3)
@@ -72,9 +65,60 @@ def main():
     printSlow(500, "You are in a room with a door and a window and a table")
     time.sleep(1)
     clear()
-    printSlow(500, "This is not sentient AI, this is just a game")
+    printSlow(1000, "---------------------------------------------------------------------------------------------------------------------------")
+    print("\n")
+    printSlow(100, "------------ ------------------ ------- | Loading Engine Logic |----    -------   - -----        ---    -         -     ---")
+    print("\n")
+    printSlow(1000, "---------------------------------------------------------------------------------------------------------------------------")
+    print("\n")
+    print("\n")
+    print("\n")
+    RandomLoad()
+    time.sleep(3)
+    clear()
+
+def on_press(key):
+    try:
+        if key.char == 't':
+            print("You made a thing!")
+            global things 
+            things += 1
+            
+        #handle ctrl+c
+        elif key.char == '~':
+            sys.exit()
+    except AttributeError:
+        pass
+
+def main():
+    # intro()
+    # -- tutorial
+    printSlow(100, "What do you do?")
+    print("\n")
+    printSlow(100, "You make things...")
+    print("\n")
+    printSlow(100, "When you have enough things, you can make more things...")
+    print("\n")
+    printSlow(100, "You can make things with the things you have")
+    printSlow(100, " and the things you make.")
+    print("\n")
+    printSlow(500, "Lets make some things!")
     time.sleep(1)
     clear()
+
+    # register the listener
+
+    listner = keyboard.Listener(on_press=on_press)
+    listner.start()
+
+    while True:
+        print("Things: " + str(things))
+        print("\n")
+        print("Press 't' to make a thing")
+        time.sleep(1)
+        clear()
+
+    
 
 if __name__ == '__main__':
     main()
