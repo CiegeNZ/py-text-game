ccbc83616089818ac4704f932c3882ec0e64382a
Conner Farrant <Conner_f@outlook.com>
2022-09-15 17:12:12 +1200

    Update game

--- window.py
+++ window.py
@@ -1,8 +1,11 @@
 from difflib import restore
+from multiprocessing.resource_sharer import stop
 import sys
 import threading
 import time
 import tkinter as tk
+
+stop = False
 
 # Class for keeping count of Things
 class Things:
@@ -25,7 +28,7 @@ class Things:
     def restore(self):
         try:
             with open("things.txt", "r") as f:
-                self.count = int(f.read())
+                self.count = float(f.read())
         except:
             pass
 
@@ -56,17 +59,18 @@ class Window(tk.Frame):
         self.add_thing.pack(side="top")
 
         self.thingMaker_count = tk.Label(self)
-        self.thingMaker_count["text"] = "ThingMaker count: " + str(thingMakers.get())
         self.thingMaker_count.pack(side="top")
 
         self.buy_thingMaker = tk.Button(self)
-        self.buy_thingMaker["text"] = "Buy a thing maker for " + str(thingMakers.cost) + " things"
         self.buy_thingMaker["command"] = self.buyThingMaker
         self.buy_thingMaker.pack(side="top")
 
         self.thingMaker_level = tk.Label(self)
-        self.thingMaker_level["text"] = "ThingMaker level: " + str(thingMakers.getLevel())
         self.thingMaker_level.pack(side="top")
+
+        self.upgrade_thingMaker = tk.Button(self)
+        self.upgrade_thingMaker["command"] = self.upgradeThingMaker
+        self.upgrade_thingMaker.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.quit)        
         self.quit.pack(side="bottom")
@@ -77,30 +81,50 @@ class Window(tk.Frame):
     def buyThingMaker(self):
         if things.get() >= thingMakers.cost:
             thingMakers.buy()
+
+    def upgradeThingMaker(self):
+        if things.get() >= thingMakers.upgradeCost:
+            thingMakers.upgrade()
         
     def update(self):
         self.thing_count["text"] = "Things: " + str(things.get())
         self.thingMaker_count["text"] = "ThingMaker count: " + str(thingMakers.get())
-        self.thingMaker_level["text"] = "ThingMaker level: " + str(thingMakers.getLevel())
+        self.thingMaker_level["text"] = "ThingMaker level: " + str(thingMakers.getLevel()) + " | Cooldown: " + str(thingMakers.delay)
+        self.buy_thingMaker["text"] = "Buy a thing maker for " + str(thingMakers.cost) + " things"
+        self.upgrade_thingMaker["text"] = "Upgrade thing maker for " + str(thingMakers.upgradeCost) + " things"
+        #disable buy if not enough things
+        if things.get() < thingMakers.cost:
+            self.buy_thingMaker["state"] = "disabled"
+        else:
+            self.buy_thingMaker["state"] = "normal"
+        #disable upgrade if not enough things
+        if things.get() < thingMakers.upgradeCost or thingMakers.get() == 0:
+            self.upgrade_thingMaker["state"] = "disabled"
+        else:
+            self.upgrade_thingMaker["state"] = "normal"
+        if thingMakers.delay < 0.3:
+            self.upgrade_thingMaker["state"] = "disabled"
+            self.upgrade_thingMaker["text"] = "ThingMaker max level"
 
     def quit(self):
         things.save()
         print("Saved things")
         thingMakers.save()
         print("Saved thingMakers")
+        global stop
+        stop = True
         self.master.destroy()
-        sys.exit(1)
 
 class ThingMaker:
     # Create thread to make things 
     def makeThings(self):
-        while True:
+        global stop
+        while stop != True:
             #handle exit command
-            if not root.winfo_exists():
-                break
             if self.level > 0:
                 time.sleep(self.delay)
                 things.add(self.count)
+                print("Made " + str(self.count) + " things")
 
     def __init__(self):
         self.level = 0
@@ -116,17 +140,19 @@ class ThingMaker:
         makeThread.start()
 
     def get(self):
-        return self.level
+        return self.count
 
     def buy(self):
         self.count += 1
+        things.sub(self.cost)
+        self.cost *= 1.2
+
     def upgrade(self):
-            if (self.delay > 0.5):
-                self.level += 1
-                self.delay -= 0.1
-                things.sub(self.upgradeCost)
-                self.upgradeCost += 5
-    
+        self.level += 1
+        self.delay -= 0.1
+        things.sub(self.upgradeCost)
+        self.upgradeCost *= 1.1
+
     def getLevel(self):
         return self.level
 
@@ -136,10 +162,10 @@ class ThingMaker:
                 data = f.read().split(':')
                 self.level = int(data[0])
                 self.count = int(data[1])
-                for i in range(self.level):
+                for _ in range(0, self.level):
                     self.delay -= 0.1
                     self.upgradeCost *= 1.1
-                for i in range(self.count):
+                for _ in range(0, self.count):
                     self.cost *= 1.2
         except:
             pass
@@ -148,13 +174,11 @@ class ThingMaker:
         with open("thingMakers.txt", "w") as f:
             f.write(str(self.level) +':'+ str(self.count))
 
-
 def main():
     global things
     things = Things()
     global thingMakers
     thingMakers = ThingMaker()
-    global root
     root = tk.Tk()
     root.geometry("400x400")
     app = Window(master=root)
